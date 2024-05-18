089fcf7490d83b23d3072c1fdad932b347f916ac
mirlan-mambetov <mirlan.zaiyrbekov@gmail.com>
2024-05-18 22:34:05 +0600

    create uplaod resource

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import * as express from 'express'
+import { join } from 'path'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.enableCors()
 
+	app.use('/uploads', express.static(join(__dirname, '..', 'uploads')))
 	const config = new DocumentBuilder()
 		.setTitle('Cloud Storage API DOCUMENTATION')
 		.setDescription('API documentation for Cloud Storage server')
