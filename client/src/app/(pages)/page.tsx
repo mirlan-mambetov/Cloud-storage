31449db82711e5735b3e7356a417768dbfb26f61
mirlan-mambetov <mirlan.zaiyrbekov@gmail.com>
2024-05-20 00:13:43 +0600

    changed patsh

--- client/src/app/(pages)/page.tsx
+++ client/src/app/(pages)/page.tsx
@@ -1,9 +1,11 @@
+import { Header } from "@/components/header/Header";
+import styles from "@/styles/page.module.css";
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Header />
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
